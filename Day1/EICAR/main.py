63445a40aad3f0033df2eb0da619d8171831071d
arad4228 <arad4228@gmail.com>
2022-09-12 15:45:59 +0900

    Update EICAR Logic And Add Dummy Test

--- Day1/EICAR/main.py
+++ Day1/EICAR/main.py
@@ -6,8 +6,19 @@ virus = 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
 vf.write(virus)
 vf.close()
 
+df = open('DummyTest.txt', 'w')
+dummy = 'Dummy Engine test file = KICOM Anti-Virus Project, 2012, Kei Choi'
+df.write(dummy)
+df.close()
+
+# 바이너리 모드로 읽기
+fp = open('DummyTest.txt', 'rb')
+fbuf = fp.read()
+fp.close()
+
 # 바이너리 모드로 읽기
 fp = open('eicat.txt', 'rb')
+# fp = open('DummyTest.txt', 'rb')
 fbuf = fp.read()
 fp.close()
 
@@ -18,5 +29,7 @@ fmd5 = m.hexdigest()
 if fmd5 == '44d88612fea8a8f36de82e1278abb02f':
     print("Virus")
     os.remove('eicat.txt')
+elif fmd5 == '77bff0b143e4840ae73d4582a8914a43':
+    print("Dummy Test Virus")
 else:
     print("No Virus")
